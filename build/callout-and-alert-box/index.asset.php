e26d560e99e8ef100903a7bec2f819480e3e4bd2
Moshtafizur-Rahman-Anty <alphaaa.90@gmail.com>
2026-05-27 23:57:06 +0600

    changed the plugin name

--- build/callout-and-alert-box/index.asset.php
+++ build/callout-and-alert-box/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '2e40f3d88057234a8f78');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '0020f6c23469e90135d5');
--- build/callout-and-alert-box/index.js
+++ build/callout-and-alert-box/index.js
@@ -338,7 +338,7 @@ module.exports = /*#__PURE__*/JSON.parse('{"$schema":"https://schemas.wp.org/tru
 /******/ 			return __webpack_require__.O(result);
 /******/ 		}
 /******/ 		
-/******/ 		var chunkLoadingGlobal = globalThis["webpackChunkcallout_and_alert_box"] = globalThis["webpackChunkcallout_and_alert_box"] || [];
+/******/ 		var chunkLoadingGlobal = globalThis["webpackChunkcallout_and_alert_box_block"] = globalThis["webpackChunkcallout_and_alert_box_block"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 /******/ 	})();
