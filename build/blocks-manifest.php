085477eaf4ccc013586ebccca097c86ced8274ac
Moshtafizur-Rahman-Anty <alphaaa.90@gmail.com>
2026-05-27 17:45:36 +0600

    first commit for our callout and alert block

---
+++ build/blocks-manifest.php
@@ -0,0 +1,47 @@
+<?php
+// This file is generated. Do not modify it manually.
+return array(
+	'callout-and-alert-box' => array(
+		'$schema' => 'https://schemas.wp.org/trunk/block.json',
+		'apiVersion' => 3,
+		'name' => 'craftblocks/callout-and-alert-box',
+		'version' => '1.0.0',
+		'title' => 'Callout & Alert Box',
+		'category' => 'text',
+		'icon' => 'megaphone',
+		'description' => 'A semantic, beautifully styled callout, alert, and notice box block for gutenberg',
+		'example' => array(
+			
+		),
+		'supports' => array(
+			'html' => false
+		),
+		'attributes' => array(
+			'content' => array(
+				'type' => 'string',
+				'default' => 'Here is the notice'
+			)
+		),
+		'styles' => array(
+			array(
+				'name' => 'info',
+				'label' => 'Info',
+				'isDefault' => true
+			),
+			array(
+				'name' => 'success',
+				'label' => 'Success'
+			),
+			array(
+				'name' => 'warning',
+				'label' => 'Warning'
+			)
+		),
+		'textdomain' => 'callout-and-alert-box',
+		'editorScript' => 'file:./index.js',
+		'editorStyle' => 'file:./index.css',
+		'style' => 'file:./style-index.css',
+		'render' => 'file:./render.php',
+		'viewScript' => 'file:./view.js'
+	)
+);
---
+++ build/callout-and-alert-box/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '784bd6ec622f5b0ee425');
